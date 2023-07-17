0c6396a33f6ca9e137f13373893110ecab1d0b96
tech189 <dlloyd189@gmail.com>
2023-07-17 22:14:35 +0100

    Progress on normalising before latin->linear b

--- test.py
+++ test.py
@@ -40,20 +40,33 @@ def latin_to_linear_b(text):
         sign_dict = json.load(sign_file)
     # text = text.split()
     print(text)
-    text = filter(None, regex.split(r"([^\p{L}0-9_-])", text))
+    text = filter(None, regex.split(r"([^\p{L}\p{M}0-9_\-\*])", text))
     # Print out the filtered text
-    for char in text:
-        print("-" + char + "-")
-        try:
-            print([unicodedata.name(c) for c in char])
-        except:
-            print("can't find char name")
+    # for char in text:
+    #     print("<" + char + ">")
+    #     try:
+    #         print([unicodedata.name(c) for c in char])
+    #     except:
+    #         print("can't find char name")
     output = ""
     # for word in text:
     #     word = word.split("-")
+    # print(list(text))
+    # TODO convert numerals !!!!!!!!!!
     for word in text:
         # print(sign_dict[syllabogram])
+        # if a word with funny  characters then  normalise
+        for char in word:
+            if not unicodedata.is_normalized("NFD", char):
+                print("<" + char + ">")
+                char_parts = unicodedata.decomposition(char).split(" ")
+                print(char_parts)
+                for part in char_parts:
+                    # first part will be normalised letter/number, second will be combining diacritic
+                    escaped = "\\u" + part
+                    print(escaped.encode().decode("unicode-escape"))
         syllabograms = word.split("-")
+        # print(syllabograms)
         for syllabogram in syllabograms:
             if syllabogram in sign_dict:
                 output = output + sign_dict[syllabogram]
@@ -80,9 +93,8 @@ def linear_b_to_latin(text):
                 output = output[-1:]
     return output
 
-print(latin_to_linear_b('''ke-re-a2 *2̣0̣1̣VAS ti-ri-po-de  
- ,  a3-ke-u  ,  ke-re-si-jo  ,  we-ke   *201VAS   2   ti-ri-po  ,  e-me  ,  po-de  ,  o-wo-we   *201VAS   1   ti-ri-po  ,  ke-re-si-jo  ,  we-ke  ,  a-pu  ,  ke-ka-u-me-ṇọ[ qe-to     *203VAS   3   di-pa  ,  me-zo-e  ,  qe-to-ro-we   *202VAS   1   di-pa-e  ,  me-zo-e  ,  ti-ri-o-we-e    *202VAS    2   di-pa  ,  me-wi-jo  ,  qe-to-ro-we     *202VAS    1    di-pa  ,  me-wi-jo  ,  ti-ri-jo-we   *202VAS   1   di-pa  ,  me-wi-jo  ,  a-no-we   *202VAS   1'''))
+print(latin_to_linear_b('''ke-re-a2 *2̣0̣1̣VAS ti-ri-po-de  ,  a3-ke-u  ,  ke-re-si-jo  ,  we-ke   *201VAS   2   ti-ri-po  ,  e-me  ,  po-de  ,  o-wo-we   *201VAS   1   ti-ri-po  ,  ke-re-si-jo  ,  we-ke  ,  a-pu  ,  ke-ka-u-me-ṇọ[ qe-to     *203VAS   3   di-pa  ,  me-zo-e  ,  qe-to-ro-we   *202VAS   1   di-pa-e  ,  me-zo-e  ,  ti-ri-o-we-e    *202VAS    2   di-pa  ,  me-wi-jo  ,  qe-to-ro-we     *202VAS    1    di-pa  ,  me-wi-jo  ,  ti-ri-jo-we   *202VAS   1   di-pa  ,  me-wi-jo  ,  a-no-we   *202VAS   1'''))
 
-print(linear_b_to_latin("𐀐𐀩𐀪𐀡"))
+# print(linear_b_to_latin("𐀐𐀩𐀪𐀡"))
 
 # print_first_decl_noun("ko-to-na")
